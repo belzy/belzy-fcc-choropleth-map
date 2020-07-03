8489d288f808fae0131c085ef00bd3d2472eddfb
brandon-elzy <brandon.g.elzy@line-zero.com>
2020-07-03 12:33:58 -0500

    Updated color range

--- client/js/main.js
+++ client/js/main.js
@@ -46,37 +46,20 @@
 
         }
 
-
-        
-
-        
-
-        
-
     }
 
     getData(dataArr => {
 
-        // console.log(dataArr);
-
-        const fipsArr = dataArr[0].map(({ fips }) => fips);
-        // console.log(fipsArr);
-
-        const countyIdArr = dataArr[1]['objects']['counties']['geometries'].map(({ id }) => id).sort((a, b) => a - b);
-        // console.log(countyIdArr);
-
-
         const countyEduData = dataArr[0];
         const topology = dataArr[1];
+        console.log(topology)
+
+        const bbox = topology['bbox'];
+        const transform = topology['transform'];
         
-        console.log(countyEduData);
-
-
         const nationGeoJson = topojson.feature(topology, topology['objects']['nation']);
         const stateGeoJson = topojson.feature(topology, topology['objects']['states']);
         const countyGeoJson = topojson.feature(topology, topology['objects']['counties']);
-        // console.log(nationGeoJson);
-        
         
         // Sort counties by ascending id.
         countyGeoJson['features'].sort((a, b) => {
@@ -86,21 +69,28 @@
             
         });
         
-        
-        
         // Add each county's education data to counties properties object
-        countyGeoJson.features.forEach(({ properties }, i) => {
-            properties = { ...countyEduData[i] }
+        countyGeoJson.features.forEach((obj, i) => {
+            countyGeoJson.features[i].properties = { ...countyEduData[i] }
         });
 
-        console.log(countyGeoJson.features);
-        
+        // Get min/max percentages.
+        // const percentages = countyEduData.map(({bachelorsOrHigher}) => bachelorsOrHigher);
+        const minPercent = d3.min(countyEduData, ({bachelorsOrHigher}) => bachelorsOrHigher);
+        const maxPercent = d3.max(countyEduData, ({bachelorsOrHigher}) => bachelorsOrHigher);
+        console.log(minPercent, maxPercent);
+
         const w = 1200;
-        const h = 600;
+        const h = 740;
+
+        // Color Scale
+        colorsArr = [/*'#F7FBFF', '#DEEBF7', '#C6DBEF', */'#9ECAE1', '#6BAED6', '#4292C6', '#2171B5', '#2171B5', '#08519C', '#08306B'];
+        const scaleColor = d3.scaleQuantize();
+        scaleColor.domain([minPercent, maxPercent]).range(colorsArr);
 
         // Define map projection
         // const projection = d3.geoAlbersUsa()
-            // .translate([w / 2, h / 2]).scale([500]);
+            // .translate([transform['translate'][0], transform['translate'][1]])//.scale([transform['scale'][0], transform['scale'][1]]);
 
         // Define default path generator
         const path = d3.geoPath()
@@ -109,10 +99,10 @@
         const svg = d3.select('#svg-choropleth-map')
             .attr('width', w)
             .attr('height', h)
-
-
-        const colorScale = d3.scaleQuantile()
-            .range(["rgb(237, 248, 233)", "rgb(186, 228, 179)", "rgb(116,196,118)", "rgb(49,163,84)", "rgb(0,109,44)"]);
+        
+        const g = svg.append('g')
+            .attr('transform', `translate(125, 100)`)
+            
 
             // svg.selectAll('path')
             //     .data(nationGeoJson.features)
@@ -123,17 +113,45 @@
             //         return path(d);
             //     })
 
-            
-
-            svg.selectAll('.county')
+            g.selectAll('.county')
                 .data(countyGeoJson.features)
                 .enter()
                 .append('path')
                 .attr('class', 'county')
-                .attr('fill', 'red')
+                .attr('fill', (d, i) => scaleColor(d['properties']['bachelorsOrHigher']))
                 .attr('d', path)
+                .attr('data-area-number', (d, i) => i)
+                .attr('data-fips', (d, i) => d['properties']['fips'])
+                .attr('data-education', (d, i) => d['properties']['bachelorsOrHigher'])
+                .on('mouseenter', (d, i) => {
 
-            svg.selectAll('.state')
+                    const properties = d['properties'];
+                    const tooltipData = `${properties['area_name']}, ${properties['state']}: ${properties['bachelorsOrHigher']}%`;
+
+                    // console.log(`${properties['area_name']}, ${properties['state']}`)
+
+                    // rect.top, rect.right, rect.bottom, rect.left
+                    const cellRect = document.querySelector(`[data-area-number="${i}"]`).getBoundingClientRect();
+
+                    const topOffset = -15;
+                    const leftOffset = 10;
+
+                    d3.select('#tooltip')
+                        .attr('data-education', () => countyGeoJson.features[i]['properties']['bachelorsOrHigher'])
+                        .style('top', `${cellRect.top + ((cellRect.bottom - cellRect.top) / 2) + topOffset}px`)
+                        .style('left', `${cellRect.right + leftOffset}px`)
+                        .style('opacity', '0.7')
+                        // .attr('data-year', dataset[i]['year'])
+
+                    d3.select('#tooltip-data')
+                        .text(tooltipData)
+                })
+                .on('mouseout', (d, i) => {
+                    d3.select('#tooltip')
+                        .style('opacity', '0.0')
+                })
+
+            g.selectAll('.state')
                 .data(stateGeoJson.features)
                 .enter()
                 .append('path')
