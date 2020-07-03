5beeee22fe12710363570ef8face2ba84803280a
brandon-elzy <brandon.g.elzy@line-zero.com>
2020-07-03 08:14:01 -0500

    Added cache system

--- client/js/main.js
+++ client/js/main.js
@@ -2,47 +2,99 @@
 
     const getData = done => {
 
+        const cacheName = 'belzy-fcc-choropleth-data-cache';
+        const cacheExpiry = 1800000; // 30 minutes in milliseconds
+        const currentTime = new Date().getTime();
+        let cache = JSON.parse(localStorage.getItem(cacheName));
         const resources = [
             'https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/for_user_education.json',
             'https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/counties.json'
         ]
-
         const init = {
             method: 'GET',
         };
 
-        fetch(resources[0], init)
+        // If data is not cached or is expired, request and cache new data.
+        if (cache === null || cache['expiration'] <= currentTime) {
+
+            if (cache === null) console.log('Cache doesn\'t exist. Creating new cache...');
+            else if (cache['expiration'] <= currentTime) console.log('Cache has expired. Updating cache...');
+
+            fetch(resources[0], init)
             .then(responseOne => responseOne.json())
             .then(responseOne => {
                 fetch(resources[1], init)
                     .then(responseTwo => responseTwo.json())
-                    .then(responseTwo => done([responseOne, responseTwo]))
+                    .then(responseTwo => {
+
+                        cache = {
+                            expiration: currentTime + cacheExpiry,
+                            data: [responseOne, responseTwo]
+                        };
+
+                        localStorage.setItem(cacheName, JSON.stringify(cache));
+
+                        done([responseOne, responseTwo])
+                    })
                     .catch(err => console.log(err));
             }).catch(err => console.log(err));      
+
+        } else {
+
+            console.log('Using cached data...');
+            done([cache['data'][0], cache['data'][1]]);
+
+        }
+
+
+        
+
+        
+
+        
 
     }
 
     getData(dataArr => {
 
-        console.log(dataArr);
+        // console.log(dataArr);
 
         const fipsArr = dataArr[0].map(({ fips }) => fips);
-        console.log(fipsArr);
+        // console.log(fipsArr);
 
         const countyIdArr = dataArr[1]['objects']['counties']['geometries'].map(({ id }) => id).sort((a, b) => a - b);
-        console.log(countyIdArr);
+        // console.log(countyIdArr);
 
 
+        const countyEduData = dataArr[0];
         const topology = dataArr[1];
+        
+        console.log(countyEduData);
 
 
         const nationGeoJson = topojson.feature(topology, topology['objects']['nation']);
         const stateGeoJson = topojson.feature(topology, topology['objects']['states']);
         const countyGeoJson = topojson.feature(topology, topology['objects']['counties']);
-        console.log(countyGeoJson);
-        console.log(nationGeoJson);
+        // console.log(nationGeoJson);
+        
+        
+        // Sort counties by ascending id.
+        countyGeoJson['features'].sort((a, b) => {
+            
+            if (a.id <= b.id) return -1;
+            else if (b.id < a.id) return 0;
+            
+        });
+        
+        
+        
+        // Add each county's education data to counties properties object
+        countyGeoJson.features.forEach(({ properties }, i) => {
+            properties = { ...countyEduData[i] }
+        });
 
-
+        console.log(countyGeoJson.features);
+        
         const w = 1200;
         const h = 600;
 
@@ -62,14 +114,33 @@
         const colorScale = d3.scaleQuantile()
             .range(["rgb(237, 248, 233)", "rgb(186, 228, 179)", "rgb(116,196,118)", "rgb(49,163,84)", "rgb(0,109,44)"]);
 
-            svg.selectAll('path')
-                .data(nationGeoJson.features)
+            // svg.selectAll('path')
+            //     .data(nationGeoJson.features)
+            //     .enter()
+            //     .append('path')
+            //     .attr('d', d => {
+            //         console.log(d)
+            //         return path(d);
+            //     })
+
+            
+
+            svg.selectAll('.county')
+                .data(countyGeoJson.features)
                 .enter()
                 .append('path')
-                .attr('d', d => {
-                    console.log(d)
-                    return path(d);
-                })
+                .attr('class', 'county')
+                .attr('fill', 'red')
+                .attr('d', path)
+
+            svg.selectAll('.state')
+                .data(stateGeoJson.features)
+                .enter()
+                .append('path')
+                .attr('class', 'state')
+                .attr('fill', 'none')
+                .attr('stroke', 'white')
+                .attr('d', path)
 
 
     });
